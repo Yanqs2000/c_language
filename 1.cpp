8632e899b6bbe039d67de8bba12fbf2bfe8feece
yanqs <1638260087@qq.com>
2024-09-13 20:29:52 +0800

    yanqs

--- 1.cpp
+++ 1.cpp
@@ -3,6 +3,6 @@
 int main() 
 {
     std::cout << "Hello, World!" << std::endl;  // 输出 "Hello, World!" 到控制台
-    std::cout << "你好，世界！" << std::endl;  // 输出 "你好，世界！" 到控制台
+    std::cout << "你好，世界！" << std::endl;// 输出 "你好，世界！" 到控制台
     return 0;  // 返回 0 表示程序正常结束
 }
