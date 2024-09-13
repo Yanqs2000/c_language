9d2b8e1de8784bed2c0c2e1c818adb0d612e06c1
yanqs <1638260087@qq.com>
2024-09-13 20:10:20 +0800

    yanqs

--- 1.cpp
+++ 1.cpp
@@ -1,6 +1,8 @@
 #include <iostream>  // 包含输入输出流库
 
-int main() {
+int main() 
+{
     std::cout << "Hello, World!" << std::endl;  // 输出 "Hello, World!" 到控制台
+    std::cout << "你好，世界！" << std::endl;  // 输出 "你好，世界！" 到控制台
     return 0;  // 返回 0 表示程序正常结束
 }
