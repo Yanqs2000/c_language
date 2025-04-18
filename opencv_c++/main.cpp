096cf7c81a8d825208c41671be0f3c5c74911670
Yanqs2000 <1638260087@qq.com>
2025-04-18 15:36:13 +0800

    yanqs

---
+++ opencv_c++/main.cpp
@@ -0,0 +1,27 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+int main() {
+    // 读取图像
+    cv::Mat img = cv::imread("/Users/yanqs/Documents/GitHub/c_language/opencv_c++/皮卡丘.png");  // 请确保这张图在程序同目录
+    if (img.empty()) {
+        std::cerr << "无法读取图片，请确保图片存在\n";
+        return 1;
+    }
+
+    // 转换为灰度图像
+    cv::Mat gray;
+    cv::cvtColor(img, gray, cv::COLOR_BGR2GRAY);
+
+    // 显示图像
+    cv::imshow("原图", img);
+    cv::imshow("灰度图", gray);
+    cv::waitKey(0); // 等待键盘输入
+
+    // 保存灰度图
+    cv::imwrite("/Users/yanqs/Documents/GitHub/c_language/opencv_c++/皮卡丘_gray.png", gray);
+
+    return 0;
+}
+
+//g++ main.cpp -o main `pkg-config --cflags --libs opencv4`
